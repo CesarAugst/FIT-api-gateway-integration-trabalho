ef1397579cc8212222b98c9907dd2d7e34f46448
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2023-06-08 13:14:13 -0300

    getPedido

--- src/controllers/PedidosController.js
+++ src/controllers/PedidosController.js
@@ -7,14 +7,14 @@ class PedidosController {
     //define o array de pedidos
     var pedidos = [];
     //se possui paginacao
-    if(per_page && page){
+    if (per_page && page) {
       //a menor pagina sempre sera a primeira
       if (page < 1) page = 1;
       //quanto pular para gerar ao primeiro registro da pagina
       var offset = (page - 1) * per_page;
       //busca todos os pedidos paginados
       pedidos = await knex("pedido").limit(per_page).offset(offset);
-    }else{
+    } else {
       //busca todos os pedidos
       pedidos = await knex("pedido");
     }
@@ -25,10 +25,12 @@ class PedidosController {
   async show(request, response) {
     //resgata numero da rota
     const { numero } = request.params;
-    //busca pedido com esse numero
-    const pedido = await knex("pedido").where({ numero }).first();
+
+    //payload e status da requisicao
+    const { status, payload } = await getPedido({numero});
+
     //retorna pedido para resposta da requisicao
-    return response.json(pedido);
+    return response.status(status).send(payload);
   }
 
   async create(request, response) {
@@ -44,19 +46,43 @@ class PedidosController {
         numero
       });
     //busca o registro criado  ocm o id da criacao
-    const pedido = await knex("pedido").where({id: pedido_id}).first();
+    const pedido = await knex("pedido").where({ id: pedido_id }).first();
     //retorna a criacao para a resposta da requisicao
     return response.json(pedido);
   }
 }
 
 //montei uma logica para fazer um numero
-function getNumero(){
+function getNumero() {
   const data = new Date(); // momento atual 
   const horas = data.getHours();
   const minutos = data.getMinutes();
   const segundos = data.getSeconds();
   return [horas, minutos, segundos].join('');
+}
+
+//logoca de busca de pedido
+async function getPedido({ numero = 0 }) {
+  //busca pedido com esse numero
+  const pedido = await knex("pedido").where({ numero }).first();
+
+  //payload da requisicao
+  var payload = pedido;
+  //status da requisicao
+  var status = 200;
+
+  //se nao encontrou nenhum pedido
+  if (typeof payload === 'undefined') {
+    //payload da resposta
+    payload = {
+      error: 'Not Found',
+      message: 'Pedido não encontrado com esse número.'
+    }
+    //status da resposta
+    status = 404;
+  } 
+  //retorna o status e o payload da requisicao
+  return { status, payload };
 }
 
 module.exports = PedidosController;
